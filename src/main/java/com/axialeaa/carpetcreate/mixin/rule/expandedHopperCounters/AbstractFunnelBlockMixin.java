600f4d8d3646548df8f62976796d4cf3737b1cd0
Axia <mcvoidredstone@gmail.com>
2024-04-09 05:10:58 +0100

    fixed unreachable code warning (which was probably fine i just don't like being screamed at because it makes me sad)

--- src/main/java/com/axialeaa/carpetcreate/mixin/rule/expandedHopperCounters/AbstractFunnelBlockMixin.java
+++ src/main/java/com/axialeaa/carpetcreate/mixin/rule/expandedHopperCounters/AbstractFunnelBlockMixin.java
@@ -18,22 +18,22 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import javax.annotation.Nullable;
+import java.util.Objects;
 
 @Mixin(AbstractFunnelBlock.class)
 public abstract class AbstractFunnelBlockMixin {
 
-	@Shadow(remap = false) @Nullable public static Direction getFunnelFacing(BlockState state) {
-		throw new AssertionError();
+	@Shadow(remap = false) public static Direction getFunnelFacing(BlockState state) {
+		return null;
 	}
 
 	@Inject(method = "tryInsert", at = @At("HEAD"), cancellable = true, remap = false)
 	private static void countOnInsert(Level worldIn, BlockPos pos, ItemStack toInsert, boolean simulate, CallbackInfoReturnable<ItemStack> cir) {
 		if (CarpetSettings.hopperCounters && CarpetCreateSettings.expandedHopperCounters) {
 			BlockState blockState = worldIn.getBlockState(pos);
-			DyeColor dyeColor = WoolTool.getWoolColorAtPosition(worldIn, pos.relative(getFunnelFacing(blockState).getOpposite()));
+            DyeColor dyeColor = WoolTool.getWoolColorAtPosition(worldIn, pos.relative(Objects.requireNonNull(getFunnelFacing(blockState)).getOpposite()));
 
-			if (dyeColor != null && !toInsert.isEmpty()) {
+            if (dyeColor != null && !toInsert.isEmpty()) {
                 HopperCounter.getCounter(dyeColor).add(worldIn.getServer(), toInsert);
 				cir.setReturnValue(toInsert);
 				toInsert.setCount(0);
